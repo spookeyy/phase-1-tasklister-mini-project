0b79e28510c1f1aa06464b07beb707b75026c178
spookeyy <pangasmeshack@gmail.com>
2024-04-01 07:53:49 +0300

    task lister mini code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+  console.log(form);
+  form.addEventListener('submit', function(event){
+  event.preventDefault();
+  let description = document.getElementById("new-task-description").value;
+
+  let div = document.getElementById("list");
+  let p = document.createElement("p")
+  div.append(p)
+  p.innerText = description
+  
+  console.log(div);
+})
+
 });
